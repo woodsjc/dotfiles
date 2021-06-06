53ea64bd3548c66a84ceb24c4bb34408d0e453c0
woodsjc <woodsjc0@gmail.com>
2021-06-05 19:47:31 -0500

    Updated diagnostic symbols and virtual text prefix. Also got tab actually working for completion. 	modified:   lua/nvim-compe.lua 	modified:   lua/nvim-lspconfig.lua

--- lua/nvim-compe.lua
+++ lua/nvim-compe.lua
@@ -23,6 +23,10 @@ require'compe'.setup {
 }
 
 --use tab
+local t = function(str)
+    return vim.api.nvim_replace_termcodes(str, true, true, true)
+end
+
 local check_back_space = function ()
     local col = vim.fn.col('.') -1
     if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
@@ -33,21 +37,25 @@ local check_back_space = function ()
 end
 
 _G.tab_complete = function()
-    if vim.fn.plumvisible() == 1 then
+    if vim.fn.pumvisible() == 1 then
         return t"<C-n>"
-    elseif check_back_space() then 
+    elseif vim.fn.call("vsnip#available", {1}) == 1 then
+        return t "<Plug>(vsnip-expand-or-jump)"
+    elseif check_back_space() then
         return t "<Tab>"
-    else 
+    else
         return vim.fn['compe#complete']()
     end
 end
 
 _G.s_tab_complete = function()
-    if vim.fn.plumvisible() == 1 then
+    if vim.fn.pumvisible() == 1 then
         return t "<C-p>"
+    elseif vim.fn.call("vsnip#jumpable", {-1}) == 1 then
+        return t "<Plug>(vsnip-jump-prev)"
     else
         return t "<S-Tab>"
-    end 
+    end
 end
 
 vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
