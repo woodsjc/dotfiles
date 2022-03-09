c9683fd7604ac93d70e108b8ddda33f80664f40a
jason <jason@solofunds.com>
2022-03-09 15:38:19 -0600

    convert to init.lua

--- lua/nvim-lspconfig.lua
+++ lua/nvim-lspconfig.lua
@@ -1,11 +1,11 @@
 local nvim_lsp = require('lspconfig')
-local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 local opts = { noremap=true, silent=true }
 
-local function on_attach()
-    buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+local function on_attach(client, buffnr)
+    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(buffnr, ...) end
+    local function buf_set_option(...) vim.api.nvim_buf_set_option(buffnr, ...) end
 
+    buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
     buf_set_keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
     buf_set_keymap("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
     buf_set_keymap("n", "gi", "<Cmd>lua vim.lsp.buf.implementation()<CR>", opts)
@@ -13,20 +13,20 @@ local function on_attach()
     buf_set_keymap("n", "<space>rr", "<Cmd>lua vim.lsp.buf.rename()<CR>", opts)
 end
 
-local servers = { "bashls", "clangd", "gopls", "pyright", "rust_analyzer",
-    "sumneko_lua", "tsserver", "yamlls" }
+local servers = { "bashls", "clangd", "gopls", "pyright", "tsserver", "yamlls" }
 for _, lsp in ipairs(servers) do
     nvim_lsp[lsp].setup { on_attach = on_attach }
 end
 
 vim.api.nvim_command("au BufWritePost *.go lua vim.lsp.buf.formatting()")
-vim.api.nvim_command("au BufWritePost *.py :!black --fast % && flake8 %")
+vim.api.nvim_command("au BufWritePost *.py :!black --line-length 100 --fast % && flake8 %")
 
 --lua specific
 local sumneko_root_path = '/Users/jason/programs/lua-language-server'
 local sumneko_binary = sumneko_root_path..'/bin/MacOS/lua-language-server'
 
 require'lspconfig'.sumneko_lua.setup {
+    on_attach = on_attach,
     cmd = {sumneko_binary, '-E', sumneko_root_path .. "/main.lua"};
     settings = {
         Lua = {
@@ -57,6 +57,28 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
         }
     }
 )
+
+--rust specific
+require'lspconfig'.rust_analyzer.setup {
+    on_attach = on_attach,
+    cmd = { "rust-analyzer" },
+    filetypes = { "rust" },
+    --root_dir = root_pattern("Cargo.toml", "rust-project.json"),
+    settings = {
+        ["rust-analyzer"] = {
+            assist = {
+                importGranularity = "module",
+                importPrefix = "by_self",
+            },
+            cargo = {
+                loadOutDirsFromCheck = true
+            },
+            procMacro = {
+                enable = true
+            },
+        }
+    }
+}
 
 vim.fn.sign_define('LspDiagnosticsSignError', { text = "✘", texthl = "LspDiagnosticsDefaultError" })
 vim.fn.sign_define('LspDiagnosticsSignWarning', { text = "", texthl = "LspDiagnosticsDefaultWarning" })
---
+++ rc_files/nvim_init.lua
@@ -0,0 +1,75 @@
+local opt = vim.opt
+
+vim.cmd([[set nocompatible]])
+vim.cmd([[filetype plugin indent on]])
+opt.syntax.enable = true
+
+opt.autowrite=true
+opt.cmdheight=2
+opt.colorcolumn="80"
+opt.completeopt="menuone,noselect"
+opt.cursorline=true
+opt.expandtab=true
+opt.hidden=true
+opt.ignorecase=true
+opt.incsearch=true
+vim.cmd([[set nobackup]])
+vim.cmd([[set noerrorbells]])
+vim.cmd([[set nolazyredraw]])
+vim.cmd([[set nohlsearch]])
+vim.cmd([[set noshowmode]])
+vim.cmd([[set noswapfile]])
+vim.cmd([[set nowrap]])
+opt.number=true
+opt.scrolloff=8
+opt.signcolumn="yes"
+opt.shortmess.c=true
+opt.showmatch=true
+opt.showmode=true
+opt.shiftwidth=4
+opt.tabstop=4
+opt.termguicolors=true
+opt.smartcase=true
+opt.updatetime=100
+opt.undodir=os.getenv("HOME").."/.vim/undodir"
+opt.undofile=true
+
+local Plug = vim.fn['plug#']
+vim.call('plug#begin', '~/.config/nvim/plugged')
+	Plug 'neovim/nvim-lspconfig'
+	Plug 'hrsh7th/cmp-nvim-lsp'
+	Plug 'hrsh7th/cmp-buffer'
+	Plug 'hrsh7th/nvim-cmp'
+	Plug 'saadparwaiz1/cmp_luasnip'
+	Plug 'L3MON4D3/LuaSnip'
+	Plug 'flazz/vim-colorschemes'
+	Plug 'nvim-lua/popup.nvim'
+	Plug 'nvim-lua/plenary.nvim'
+	Plug 'nvim-telescope/telescope.nvim'
+	Plug('nvim-treesitter/nvim-treesitter', {['do'] = ':TSUpdate'})
+	Plug 'simrat39/rust-tools.nvim'
+vim.call('plug#end')
+
+vim.cmd([[colorscheme seattle]])
+
+vim.g.ftplugin_sql_omni_key = '<C-P>q'
+vim.g.python3_host_prog = '/usr/local/opt/python@3.7/bin/python3.7'
+
+vim.api.nvim_set_keymap('n', '<Space>', '<Nop>', {noremap=true})
+vim.g.mapleader = " "
+
+vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<cr>', {noremap=true})
+vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>Telescope live_grep<cr>',  {noremap=true})
+vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>Telescope buffers<cr>',    {noremap=true})
+vim.api.nvim_set_keymap('n', '<leader>fh', '<cmd>Telescope help_tags<cr>',  {noremap=true})
+vim.api.nvim_set_keymap('n', '<leader>bd', '<cmd>bd<cr>',                   {noremap=true})
+vim.api.nvim_set_keymap('n', '<leader>bn', '<cmd>bn<cr>',                   {noremap=true})
+vim.api.nvim_set_keymap('n', '<leader>bp', '<cmd>bp<cr>',                   {noremap=true})
+
+vim.cmd([[au BufReadPost *.jet set syntax=html]])
+vim.cmd([[au BufEnter *.rs colorscheme deep-space]])
+
+require('treesitter')
+require('nvim-lspconfig')
+require('nvim-cmp')
+require('rust-tools').setup({})
